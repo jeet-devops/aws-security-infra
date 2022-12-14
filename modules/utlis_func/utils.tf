5eb52c9379a59480fcfee690c53231e3e5020fd7
jeet-devops <53583887+jeet-devops@users.noreply.github.com>
2022-12-14 16:50:07 -0500

    lower random string values

--- modules/utlis_func/utils.tf
+++ modules/utlis_func/utils.tf
@@ -1,10 +1,10 @@
 resource "random_string" "random" {
-  length           = 6
+  length           = 10
   special          = false
   upper = false
 }
 
 output "random_string" {
 
-    value     = "${random_string.random.result}-bucket"
+    value     = lower("${random_string.random.result}-bucket")
 }
